a0279c7ca690fe218d4155b860a28414550b5a92
Madi4ar <133092635+Madi4ar@users.noreply.github.com>
2023-06-22 22:38:14 +0600

    Add files via upload

---
+++ src/views/CityView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Suspense>
+            <AsyncCityView />
+            <template #fallback>
+                <p>Loading...</p>
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+<script setup>
+import { Suspense } from 'vue';
+import AsyncCityView from './AsyncCityView.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
